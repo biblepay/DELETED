4380f39ca42647fbd20129bd9acd4dfa56b0d6c5
biblepay <contact@biblepay.org>
2019-09-16 10:57:12 -0500

    2.0.3.2-Pool
    - Remove deprecated files
    - Add missing bitnet files

---
+++ BiblePayPool2018/bitnet/Bitnet/Program.cs
@@ -0,0 +1,24 @@
+// COPYRIGHT 2011 Konstantin Ineshin, Irkutsk, Russia.
+// If you like this project please donate BTC 18TdCC4TwGN7PHyuRAm8XV88gcCmAHqGNs
+
+using System;
+using System.Collections;
+using System.Net;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+using Bitnet.Client;
+
+
+namespace Bitnet
+{
+  internal sealed class Program
+  {
+        
+    [STAThread]
+    static void Main()
+    {
+    }
+  }
+}
